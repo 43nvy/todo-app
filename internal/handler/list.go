3ece870c7410346690622efc9d41a646925591b3
Andrey Danko <tor.andrey4312@mail.ru>
2023-11-29 19:41:21 +0300

    Add JWT authorization middleware for APIs endpoin group

--- internal/handler/list.go
+++ internal/handler/list.go
@@ -1,23 +1,31 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
 
-func (h *Handler) createList(c *gin.Context) {
+	"github.com/gin-gonic/gin"
+)
 
-}
-
-func (h *Handler) getAllLists(c *gin.Context) {
-
-}
-
-func (h *Handler) getListById(c *gin.Context) {
-
-}
-
-func (h *Handler) udpateList(c *gin.Context) {
+func (h *Handler) createList(ctx *gin.Context) {
+	id, _ := ctx.Get(userCtx)
+	ctx.JSON(http.StatusOK, map[string]interface{}{
+		"id": id,
+	})
 
 }
 
-func (h *Handler) deleteList(c *gin.Context) {
+func (h *Handler) getAllLists(ctx *gin.Context) {
+
+}
+
+func (h *Handler) getListById(ctx *gin.Context) {
+
+}
+
+func (h *Handler) udpateList(ctx *gin.Context) {
+
+}
+
+func (h *Handler) deleteList(ctx *gin.Context) {
 
 }
